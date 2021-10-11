80fe6a8bdd04e4eb717611e637dbee95bb9df924
Ciensprog <jorg.leonrd@gmail.com>
2021-10-11 12:56:37 -0600

    Add option base with choices

---
+++ src/commons/SlashCommandOptionWithChoices.ts
@@ -0,0 +1,63 @@
+import { isArray, isEmpty } from 'lodash';
+
+import {
+  ApplicationCommandOptionChoiceStructure,
+  ApplicationCommandOptionStructure,
+  ApplicationCommandOptionTypes,
+  Choices,
+} from '@src/interfaces';
+
+import SlashCommandOption from '@common/SlashCommandOption';
+
+/*
+|--------------------------------------------------------------------------
+| SlashCommandBuilder::Commons -> SlashCommandOptionWithChoices
+|--------------------------------------------------------------------------
+|
+| ...
+|
+*/
+
+export default class SlashCommandOptionWithChoices<A extends number | string> extends SlashCommandOption {
+  private static readonly MAX_ITEMS: number = 25;
+
+  constructor(type: ApplicationCommandOptionTypes) {
+    super(type);
+  }
+
+  public readonly choices: Array<ApplicationCommandOptionChoiceStructure> = [];
+
+  public addChoice(name: string, value: A): this {
+    const maxItems: number = SlashCommandOptionWithChoices.MAX_ITEMS - 1;
+
+    if (this.choices.length > maxItems) {
+      throw new Error(`Choices cannot exceed ${SlashCommandOptionWithChoices.MAX_ITEMS} items`);
+    }
+
+    Reflect.set(this, 'choices', [
+      ...this.choices,
+      { name, value },
+    ]);
+
+    return this;
+  }
+
+  public addChoices(choices: Array<Choices<A>>): this {
+    if (isEmpty(choices) || !isArray(choices)) {
+      throw new Error('The `value` parameter is not an array or is empty');
+    }
+
+    for (const { name, value } of choices) {
+      this.addChoice(name, value);
+    }
+
+    return this;
+  }
+
+  public override toJSON(): ApplicationCommandOptionStructure {
+    return {
+      ...super.toJSON(),
+      choices: this.choices,
+    };
+  }
+}
